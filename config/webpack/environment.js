61101706a259b49f66cc96e01a7c49169b9261c5
Roberto Quintanilla <roberto.quintanilla@gmail.com>
2020-08-08 15:27:20 -0500

    Make bootstrap work..Make bootstrap work....
    https://dev.to/somnathpaul/add-bootstrap-4-to-your-ruby-on-rails-6-application-ole

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,13 @@
 const { environment } = require('@rails/webpacker')
 
+// Include jQuery & popper.js: =================================================
+const webpack = require('webpack')
+
+environment.plugins.append('Provide', new webpack.ProvidePlugin({
+  $: 'jquery',
+  jQuery: 'jquery',
+  Popper: ['popper.js', 'default']
+}))
+// =============================================================================
+
 module.exports = environment
