0731c5f588177ac8d3921af581b3289393c2941f
Roberto Quintanilla <roberto.quintanilla@gmail.com>
2021-01-14 00:17:50 -0600

    Load swarm/kubernetes secrets

--- config/boot.rb
+++ config/boot.rb
@@ -1,4 +1,6 @@
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
 
+require 'on_container/load_env_secrets' # Load secrets injected by Kubernetes/Swarm
+
 require 'bundler/setup' # Set up gems listed in the Gemfile.
 require 'bootsnap/setup' # Speed up boot time by caching expensive operations.
