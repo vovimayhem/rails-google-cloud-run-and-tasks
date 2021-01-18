2a2074a31ac879306887239f9d5b7a40b7a10d92
Roberto Quintanilla <roberto.quintanilla@gmail.com>
2021-01-17 22:03:14 -0600

    Sign in using Firebase authentication

--- app/javascript/controllers/sign-in_controller.js
+++ app/javascript/controllers/sign-in_controller.js
@@ -8,11 +8,32 @@
 // </div>
 
 import { Controller } from "stimulus"
+import firebase from "firebase/app"
+import "firebase/auth"
 
 export default class extends Controller {
-  static targets = [ "output" ]
+  static targets = [ "email", "password", "output", "submit" ]
 
   connect() {
     this.outputTarget.textContent = 'Hello, Stimulus is Online!'
+  }
+
+  submit(event) {
+    event.preventDefault()
+    this.outputTarget.textContent = 'Signing you in...'
+
+    const email = this.emailTarget.value
+    const password = this.passwordTarget.value
+    
+    firebase.auth().signInWithEmailAndPassword(email, password)
+    .then((user) => {
+      debugger // TODO: Verify ID Token - see https://firebase.google.com/docs/auth/admin/verify-id-tokens
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      this.outputTarget.textContent = `${errorMessage} (${errorCode})`
+      this.submitTarget.disabled = false
+    });
   }
 }
